a199fa826886567409fd29cccaed3cab00be58df
EyesIsMine <otherthing6@gmail.com>
2011-08-13 12:40:49 +0100

    Fixed MSG and attempted parameters

--- modules/msg.py
+++ modules/msg.py
@@ -13,10 +13,11 @@ class Msg(handler.Handler):
 		msg_words = msg.split(' ')
 		nick = line.split(':')[1].split('!')[0]
 		target = words[2]
+		msg_stuff = words[2]
 
 		if target.find('#') != 0:
 			target = nick
 		
 		if len(msg_words) >= 1:
-			if self.commands.getcmd(msg_words[0], 'msg'):
-				self.commands.privmsg(target, "%s: MSG" % nick)
+			if self.commands.getcmd(msg_words[0], 'msg', msg_stuff):
+				self.commands.privmsg(target, "%s: %d" % (nick, msg_stuff))
