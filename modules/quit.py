9b6bb1b6b99f0c8073e96009e26f8a12f1fdbe23
Daniel Bugl <Daniel.Bugl@gmail.com>
2011-08-13 12:32:14 +0200

    r2 - Added a quit/restart module

---
+++ modules/quit.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+#coding: utf8
+
+
+import handler
+
+
+class Quit(handler.Handler):
+	def privmsg(self, words):
+		line = ' '.join(words)
+		msg = line.split(':')[2]
+		msg_words = msg.split(' ')
+		nick = line.split(':')[1].split('!')[0]
+		target = words[2]
+
+		if target.find('#') != 0:
+
+			target = nick
+		
+		if len(msg_words) >= 1 and self.commands.getrank(nick) >= 6:
+			if self.commands.getcmd(msg_words[0], 'quit') == 1:
+				self.commands.notice(target, "Okay... Bye :)")
+				self.commands.quit("Quit command")
+				self.commands.disconnect()
+			if self.commands.getcmd(msg_words[0], 'restart') == 1:
+				self.commands.notice(target, "I'm restarting :)")
+				self.commands.quit("Restarting...")
+				self.commands.disconnect()
+				self.commands.reconnect()
