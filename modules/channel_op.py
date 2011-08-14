db8910211321f6b856b307205cb724e0c4c82e0c
EyesIsMine <otherthing6@gmail.com>
2011-08-14 14:14:40 +0100

    r15 - Added unban

--- modules/channel_op.py
+++ modules/channel_op.py
@@ -74,7 +74,13 @@ class Channel_op(handler.Handler):
 			if msg_words[0] == temp_str:
 				if len(msg_words) >= 2:
 					self.commands.kick(target, msg_words[1], msg_words[2])
-					self.commands.mode("+b %s!*@*" % msg_words[1], target)
+		                        self.commands.mode("+b %s!*@*" % msg_words[1], target)
+			temp_str = self.client.properties.get('prefix')
+			temp_str += 'unban'
+			if msg_words[0] == temp_str:
+				if len(msg_words) >= 2:
+					self.commands.mode("-b %s!*@*" % msg_words[1], target)
+
 		else:
 			temp_str = self.client.properties.get('prefix')
 			temp_str += 'kick'
