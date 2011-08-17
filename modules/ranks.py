53f575dccd7ccd71f1b3b7e7a0daee26d63a3d0b
Daniel Bugl <Daniel.Bugl@gmail.com>
2011-08-17 19:33:46 +0200

    Added an 'addowner' command with verification codes to the ranks module.

--- modules/ranks.py
+++ modules/ranks.py
@@ -4,15 +4,41 @@
 
 
 import handler
-
+import string
+import random
 
 class Ranks(handler.Handler):
+	def addowner(self, nick):
+		global string, random
+		chars = string.letters + string.digits
+		code = ""
+		for i in range(10):
+			code = code + random.choice(chars)
+		self.code = code
+		print "Your owner code is: '%s'. Please PM your bot: 'addowner %s <YOURNAME>'. Replace <YOURNAME> with your nick on IRC." % (self.code, self.code)
+		self.commands.privmsg(nick, "By the way, you need to verify that you are the real owner of the bot, please look at the bots console for a verification code.")
+
 	def privmsg(self, words):
 		line = ' '.join(words)
 		msg = line.split(':')[2]
 		msg_words = msg.split(' ')
 		nick = line.split(':')[1].split('!')[0]
 		target = words[2]
+
+		if target == self.client.nick:
+			if len(msg_words) >= 3:
+				if msg_words[0] == "addowner":
+					self.addowner(nick)
+					if msg_words[1] == self.code:
+						self.commands.setrank(msg_words[2], 7)
+						self.commands.privmsg(nick, "Successfully added '%s' as bot-owner." % msg_words[2])
+					else:
+						self.commands.privmsg(nick, "Wrong code.")
+				else:
+					self.commands.privmsg(nick, "Sorry, I do not have this command in my database. You may want to try 'addowner <CODE> <USERNAME>'.")
+			elif len(msg_words) >= 1 and msg_words[0] == "addowner":
+				self.addowner(nick)
+				self.commands.privmsg(nick, "Not enough arguments, try: 'addowner <CODE> <USERNAME>'.")
 
 		if target.find('#') != 0:
 			target = nick
