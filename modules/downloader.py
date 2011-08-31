1c37bb3e6ae862823ff502b6703dabd231464499
Isaac F. D <otherthing6@gmail.com>
2011-08-31 12:20:41 +0100

    r33 - Module downloader.

--- modules/downloader.py
+++ modules/downloader.py
@@ -4,7 +4,6 @@
 
 
 import handler
-import urllib2
 
 class Downloader(handler.Handler):
 	def privmsg(self, words):
@@ -16,16 +15,18 @@ class Downloader(handler.Handler):
 
 		if target.find('#') != 0:
 			target = nick
-		
-		if len(msg_words) >= 1:
+		if len(msg_words) >= 0:
 			if self.commands.getcmd(msg_words[0], 'instmodule'):
-				if len(msg_words) == 0:
-					self.commands.notice(nick, "Usage: %sinstmodule <modulename> - Installs the selected module from the iFB module repository." % self.client.properties.get('prefix'))
-				elif len(msg_words) == 1:
+				if len(msg_words) <= 1:
+					self.commands.notice(nick, "Usage: instmodule <modulename> - Installs the selected module from the iFB module repository.")
+				elif len(msg_words) >= 1:
+					import urllib2
 					try:
 						moduledown = urllib2.urlopen("http://www.wiseeyes.co.cc/ifb/modules/%s.py" % msg_words[1])
-						output = open('%s.py' % msg_words[1], 'wb')
+						output = open('modules/%s.py' % msg_words[1], 'wb')
 						output.write(moduledown.read())
-						output.close				
+						output.close()
+						self.client.load_module(msg_words[1])
+						self.commands.notice(nick, "Module %s downloaded and loaded." % msg_words[1])
 					except IOError:
-						self.commands.notice(nick, "ERROR: 404 module not found")
+						self.commands.notice(nick, "ERROR: Module not found.")
